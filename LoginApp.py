d79906f97d35960c66c69d2c51467c6ba81b13a8
Guamss <guamsmdr@gmail.com>
2021-06-18 16:13:45 +0200

    LoginApp v0.3

--- LoginApp.py
+++ LoginApp.py
@@ -1,12 +1,7 @@
 from kivy.app import App
-from kivy.config import Config
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 import mysql.connector
-from mysql.connector import cursor
 
 cnx = mysql.connector.connect(
     host="localhost",
@@ -16,53 +11,23 @@ cnx = mysql.connector.connect(
     )
 mycursor = cnx.cursor()
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        
-        self.cols = 1
-        
-        self.inside= GridLayout()
-        self.inside.cols = 2
-        self.title = "Login"
-        
-        self.inside.add_widget(Label(text="Username :", font_size="60px"))
-        self.username = TextInput(multiline=False)
-        self.inside.add_widget(self.username)
-
-        self.inside.add_widget(Label(text="Password :", font_size="60px"))
-        self.password = TextInput(multiline=False)
-        self.inside.add_widget(self.password)
-        
-        self.add_widget(self.inside)
-        
-        self.login = Button(text="Login", font_size="60px")
-        self.login.bind(on_press=self.pressed)
-        self.add_widget(self.login)
-        self.contentpopup = Button(text="Close popup")
-        self.error = Popup(title="Error ! It seems you spelled something wrong...",
-            content=self.contentpopup)
-        self.contentpopup.bind(on_press=self.error.dismiss)
-        
-    def pressed(self, instance):
+class LoginApp(Widget):
+    username = ObjectProperty(None)
+    password = ObjectProperty(None)
+    
+    def btn(self):
         username = self.username.text
         password = self.password.text
         mycursor.execute("SELECT * FROM User")
         users = mycursor.fetchall()
-        #BOULE FOR NON COMPLETE ! UNIQUEMENT LE IF MARCHE
-        #(Uniquement une connexion avec les 
-        #bons identifiants est fonctionnelle)
+        print(f"username : {username} password : {password}")
         for user in users:
             if username == user[0] and password == user[1]:
-                print("Vous êtes connecté!")
                 self.username.text = ""
                 self.password.text = ""
            
-class LoginApp(App):
+class MyApp(App):
     def build(self):
-        return MyGrid()
-
-Config.set("graphics", "width", 700)
-Config.set("graphics", "height", 720)
+        return LoginApp()
     
-LoginApp().run()
+MyApp().run()
